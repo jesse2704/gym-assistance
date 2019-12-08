e2333402a8df2e7a0ccf51b56a6006f3c11a2443
Jesse Akoma <jesse2704@gmail.com>
2019-12-08 13:18:04 +0100

    Added login functionality TODO: Valid user input, show error messages

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import 
 
 class home extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.logout = this.logout.bind(this);
     }
+
+
+    logout() {
+        fire.auth().signOut();
+    }
+
     render() {
         return (
             <div>
                 <h1>You are home</h1>
+                <button onClick={this.logout}>logout</button>
             </div>
         )
     }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -10,7 +10,7 @@ class Login extends Component {
     this.signup = this.signup.bind(this);
     this.state = {
       email: '',
-      password: ''
+      password: '',
     };
   }
 
@@ -22,7 +22,7 @@ class Login extends Component {
     e.preventDefault();
     fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
     }).catch((error) => {
-        console.log(error);
+        console.log(error)
       });
   }
 
@@ -40,14 +40,14 @@ class Login extends Component {
        <form>
       <div class="form-group">
        <label for="exampleInputEmail1">Email address</label>
-       <input value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-       <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+       <input value={this.state.email} onChange={this.handleChange} type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+       <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
-       <div class="form-group">
+       <div className="form-group">
       <label for="exampleInputPassword1">Password</label>
-      <input value={this.state.password} onChange={this.handleChange} type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+      <input value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
       </div>
-      <button type="submit" onClick={this.login} class="btn btn-primary">Login</button>
+      <button type="submit" onClick={this.login} className="btn btn-primary">Login</button>
       <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
  </form>
  
