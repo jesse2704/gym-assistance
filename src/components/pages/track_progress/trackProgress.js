270b3e4a8fd8e1228670e19f3491d83cd75499c5
jesse2704 <jesse2704@users.noreply.github.com>
2019-12-11 13:52:21 +0100

    Added 3 navbar links

---
+++ src/components/pages/track_progress/trackProgress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function trackProgress() {
+    return (
+        <div>
+            <h1>Track page</h1>
+        </div>
+    )
+}
+
+export default trackProgress
---
+++ src/components/pages/user_profile/userProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function userProfile() {
+    return (
+        <div>
+            <h1>User page</h1>
+        </div>
+    )
+}
+
+export default userProfile
